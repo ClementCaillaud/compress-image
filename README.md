ee0b54a53d726a210fe20c32927c39bae5f5c588
ClementCaillaud <34067765+ClementCaillaud@users.noreply.github.com>
2019-02-26 11:10:54 +0100

    Add script v1

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # compress-image
 Compress an image in JavaScript
+
+## What it does
+This code was made to reduce a file input size before send it to the server. It uses canvas and blob to lower image quality. You can choose the quality level so feel free to test different values and see the result.
+
+## Installation
+Include `compress-image.js` in your project, with the path of the folder in which this file is located.
+```html
+<script src="compress-image.js"></script>
+```
+
+## Usage
+First of all, declare a new **CompressImage** object :
+```javascript
+const ci = new CompressImage();
+```
+This object give you access to the **compress** function, which takes an object in parameter.
+```javascript
+ci.compress(
+{
+	image: , 	//The file you want to compress
+	quality: , 	//Image quality, a number between 0 and 1 (default 0.5)
+	success: , 	//Callback function, takes compressed image as parameter
+	error: 		// Callback function when en error occurs, takes the error in parameter
+});
+```
+The image should come from a file input.
+Compressed image will be returned in the success callback function.
+
+## Example
+
+### index.html
+```html
+<script src="compress-image.js"></script>
+<script src="script.js"></script>
+
+<input type="file" id="file" accept=".png, .jpeg, .jpg" multiple onchange="compress_file(this)"/>             
+```
+
+### script.js
+```javascript
+function compress_file(input)
+{
+	//Get the files from the input
+	var files = input.files;
+	var l = files.length;
+
+	//Create CompressImage object
+	const ci = new CompressImage();
+
+	//Loop for each image from the input
+	for(var i = 0; i < l; i++)
+	{
+		var picture = files[i];
+
+		//Compress picture with a quality of 0.5
+		ci.compress(
+		{
+			image: picture,
+			quality: 0.5,
+			success: function(compressedPicture)
+			{
+			  //DO SOMETHING
+			},
+			error: function(e)
+			{
+			  console.log(e);
+			  //OR DO SOMETHING ELSE
+			}
+		});
+
+	}
+}
+```
+## Licence
+This project is licensed under the MIT License - see the [LICENSE](https://github.com/ClementCaillaud/compress-image/blob/master/LICENSE) file for details.
+Feel free to modify and improve this script.
---
+++ compress-image.js
@@ -0,0 +1,118 @@
+/**
+ * @description Compress an image
+ * @author Clément CAILLAUD
+ */
+
+function CompressImage()
+{
+  /* PUBLIC */
+
+  /**
+   * Compress an image
+   * @param  {Object}   param     image     :   File      ->  the image we compress,
+   *                              quality   :   number    ->  the quality of the compressed image, float between 0 and 1 (default 0.5)
+   *                              success   :   function  ->  the function to call when process is done, take the compressed image as parameter
+   *                              error     :   function  ->  the function to call when we got error, take the error feedback in parameter
+   */
+  this.compress = function(param)
+  {
+    try
+    {
+      param = checkParam(param);
+      startCompress(param.image, param.quality, param.success);
+    }
+    catch (e)
+    {
+      if(typeof param.error === 'function')
+      {
+        param.error(e);
+      }
+    }
+  };
+
+  /* PRIVATE */
+
+  /**
+   * Check parameters and set default, or throw exception if there is error
+   * @param  {object} param Object containing parameters
+   * @return {object}       Parameters with correct values
+   */
+  var checkParam = function(param)
+  {
+    // Check if param is an Object
+    if( typeof param !== 'object' )
+    {
+      throw Error('Param in compress function is not an object. It should be like {image : File, [optional] minWidth : Number, [optional] divScale : Number}, success : Function, error : Function');
+    }
+
+    // Check if image is a File
+    if( !param.image instanceof File )
+    {
+      throw Error('Image in compress function is not a File object');
+    }
+
+    // Check if quality is a number between 0 and 1, default 0.5
+    if( typeof param.quality === 'number' )
+    {
+      if( param.quality < 0 || param.quality > 1 )
+      {
+        throw Error('Quality must be between 0 and 1');
+      }
+    }
+    else
+    {
+      param.quality = 0.5;
+    }
+
+    return param;
+  };
+
+  /**
+   * Start image compression
+   * @param  {File}     image     Image to compress
+   * @param  {Number}   quality   The quality of the image (0 ~ 1)
+   * @param  {Function} success   The function to be called when process ending
+   */
+  var startCompress = function(image, quality, success)
+  {
+    const reader = new FileReader();
+    reader.readAsDataURL(image);
+
+    //Read File
+    reader.onload = function(event)
+    {
+      const img = new Image();
+      img.src = event.target.result;
+
+      //Read image from file
+      img.onload = function()
+      {
+        const elem = document.createElement('canvas');
+        elem.width = img.width;
+        elem.height = img.height;
+
+        const ctx = elem.getContext('2d');
+        ctx.drawImage(img, 0, 0, elem.width, elem.height);
+
+        //Create the compressed file
+        ctx.canvas.toBlob(function(blob)
+        {
+          var compressedImage = new File(
+            [blob],
+            image.name,
+            {
+              type: image.type,
+              lastModified: Date.now()
+            }
+          );
+
+          if(typeof success === "function")
+          {
+            success(compressedImage);
+          }
+
+        }, image.type, quality);
+      };
+    };
+  };
+}
